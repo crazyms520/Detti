fe1863cc75f6487ebd8a9a113df924f14e965654
OA Wu <comdan66@gmail.com>
2019-07-13 22:09:34 +0800

    上傳前壓縮紀錄。

--- js/public.js
+++ js/public.js
@@ -1,14 +1,1 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2019, Ginkgo
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
- 
-$(function() {
-  $('*[data-bgurl]').each(function() {
-    var $that = $(this);
-    var url = $that.data('bgurl');
-    $that.css({'background-image': 'url(' + (url.length ? url : '') + ')'});
-  });
-});
+$(function(){$("*[data-bgurl]").each(function(){var a=$(this),n=a.data("bgurl");a.css({"background-image":"url("+(n.length?n:"")+")"})})});
